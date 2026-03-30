0acf24ed9af987a17b70fada212640255a9e9773
aadiwaykole <adityawaykole494@gmail.com>
2026-03-31 00:24:52 +0530

    add ApiError and ApiResponse utility classes

---
+++ first/src/utils/ApiError.js
@@ -0,0 +1,22 @@
+class AipError extends Error{
+    constructor(
+        statusCode,
+        message= "something went wrong",
+        error = [],
+        stack=""
+    ) {
+        super(message);
+        this.statusCode = statusCode;
+        this.data  = null
+        this.message = message;
+        this.success =false ;
+        this.errors = this.error;
+
+
+        if(statck){
+            this.stack = stack;
+        }else{
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
